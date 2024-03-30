ffc55733d069a4f63b9e5dde5cf5619b3b5d0f96
Pavel Vakulich <pavelvakulich@gmail.com>
2024-03-30 12:29:07 +0300

    debug

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,28 +1,35 @@
-import { Breadcrumb, Button, Layout } from 'antd';
+import { Breadcrumb, Layout } from 'antd';
 import styles from './Header.module.css';
 import Title from 'antd/lib/typography/Title';
 // import { SettingOutlined } from '@ant-design/icons';
 // import { useMediaQuery } from 'react-responsive';
 import { Link } from 'react-router-dom';
-import { PATHS } from '@constants/paths';
+
 import { SettingsButton } from '@components/buttons/settings-button';
 
-export const Header = () => {
-    return (
-        <Layout.Header className={styles.header}>
-            <Breadcrumb className={styles.breadcrumbs}>
-                <Breadcrumb.Item className={styles.breadcrumbs_item}>
-                    <Link to={PATHS.MAIN}>Главная</Link>
-                </Breadcrumb.Item>
-            </Breadcrumb>
-            <div className={styles.horizontal_layout}>
-                <Title level={1} className={styles.title}>
-                    Приветствуем тебя в&nbsp;CleverFit&nbsp;— приложении,
-                    <br /> которое поможет тебе добиться своей мечты!
-                </Title>
-
-                <SettingsButton />
-            </div>
-        </Layout.Header>
-    );
+type HeaderProps = {
+    title?: React.ReactNode | string;
+    breadCrumbs?: [{ title: string; link: string }];
 };
+
+export const Header = ({ breadCrumbs, title }: HeaderProps) => (
+    <Layout.Header className={styles.header}>
+        <Breadcrumb className={styles.breadcrumbs}>
+            {breadCrumbs?.map((crumb) => (
+                <Breadcrumb.Item className={styles.breadcrumbs_item} key={crumb.link}>
+                    <Link to={crumb.link}>{crumb.title}</Link>
+                </Breadcrumb.Item>
+            ))}
+        </Breadcrumb>
+        <div className={styles.horizontal_layout}>
+            {title && (
+                <Title level={1} className={styles.title}>
+                    {title}
+                </Title>
+            )}
+            <Title level={1} className={styles.title} />
+
+            <SettingsButton />
+        </div>
+    </Layout.Header>
+);
